07fee09431d390e0a1967e6995d963f1dc538ae1
jayashri1983 <jayashri.phonde@dmce.ac.in>
2025-08-05 19:46:31 +0530

    Update login.py

--- login.py
+++ login.py
@@ -2,7 +2,7 @@ import os
 
 def run_command():
     user_input = input("Enter a command: ")
-    os.system(user_input)  # 🚨 Vulnerable: command injection
+    os.system(user_input)  # 🚨 Vulnerable: command injection 
 
 if __name__ == "__main__":
     run_command()
